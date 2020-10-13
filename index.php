2d8a73650ffcb1bb9fad2bb62bddecf08c714b6b
Eric Kennedy <erickennedy@rogers.com>
2020-10-13 16:01:30 -0400

    Revert "commit on css-edtis"
    This reverts commit 2403448cd9e86894fff613d8b1ded17110630728.

--- index.php
+++ index.php
@@ -1,21 +1,15 @@
 <html>
     <head>
         <title>Pizza Project</title>
-        <meta charset="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css/main.css"/>
+        <h1>Welcome to Pizza</h1>
     </head>
     <body>
-    <div class="container">
-        <div class="main">
-        <h1>Welcome to Pizza</h1>
         <p>This app helps with the ordering of pizza</p>
         <form action="/orderpizza.php">
             <label for="email">Email:</label>
             <input type="text" id="email" name="email">
             <input type="submit" value="Submit">
         </form>
-        </div>
     </body>
-    </div>
+
 </html>
