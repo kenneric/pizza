d11eeab1b4e286720a5fb9cb975a4a211ed0b433
Eric Kennedy <erickennedy@rogers.com>
2020-10-13 16:07:54 -0400

    commit on main

--- orderpizza.php
+++ orderpizza.php
@@ -1,6 +1,15 @@
 <html>
     <head>
         <title>Pizza Project</title>
-        <h1>Coming Soon!</h1>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>
     </head>
+    <body>
+        <div class="container">
+            <div class="main">
+                <h1>Coming soon!</h1>
+            </div>
+        </div>
+    </body>
 </html>
