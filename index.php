bbb7f7202cd0296f16917a04404ad1ef4fe20977
Eric Kennedy <erickennedy@rogers.com>
2020-10-13 16:04:57 -0400

    commit on main

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/main.css"/>
-        <h1>Welcome to Pizza</h1>
     </head>
     <body>
     <div class="container">
