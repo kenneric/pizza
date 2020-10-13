2403448cd9e86894fff613d8b1ded17110630728
Eric Kennedy <erickennedy@rogers.com>
2020-10-13 16:01:22 -0400

    commit on css-edtis

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <html>
     <head>
         <title>Pizza Project</title>
-        <h1>Welcome to Pizza</h1>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>
     </head>
     <body>
+    <div class="container">
+        <div class="main">
+        <h1>Welcome to Pizza</h1>
         <p>This app helps with the ordering of pizza</p>
         <form action="/orderpizza.php">
             <label for="email">Email:</label>
             <input type="text" id="email" name="email">
             <input type="submit" value="Submit">
         </form>
+        </div>
     </body>
-
+    </div>
 </html>
